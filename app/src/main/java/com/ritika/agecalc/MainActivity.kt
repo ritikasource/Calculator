21a7ace0990d3f8d4470804aa1c0e0fc0b8b865f
ritikasource <ritika99.work@gmail.com>
2019-09-12 21:31:26 +0530

    Change the onclick function specification with each number

--- app/src/main/java/com/ritika/agecalc/MainActivity.kt
+++ app/src/main/java/com/ritika/agecalc/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.ritika.agecalc
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
@@ -18,12 +17,81 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+        fun buClick(view:View)
+        {
+            val buSel=view as Button
+
+            var buVal:String=etShow.text.toString()
+
+            when(buSel.id)
+            {
+                button0.id->
+                {
+                    buVal+="0"
+
+                }
+                button1.id->
+                {
+                    buVal+="1"
+
+                }
+                button2.id->
+                {
+                    buVal+="2"
+
+                }
+                button3.id->
+                {
+                    buVal+="3"
+
+                }
+                button4.id->
+                {
+                    buVal+="4"
+
+                }
+                button5.id->
+                {
+                    buVal+="5"
+
+                }
+                button6.id->
+                {
+                    buVal+="6"
+
+                }
+                button7.id->
+                {
+                    buVal+="7"
+
+                }
+                button8.id->
+                {
+                    buVal+="8"
+
+                }
+                button9.id->
+                {
+                    buVal+="9"
+
+                }
+                buttonp.id->
+                {
+                    buVal+="-" + buVal
+
+                }
+                buttond.id->
+                {
+                    buVal+="."
+
+                }
+
+            }
+            etShow.setText(buVal)
         }
 
-        }
+
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -40,78 +108,5 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
-    fun buClick(view:View)
-    {
-        val buSel=view as Button
-
-        var buVal:String=etShow.text.toString()
-
-        when(buSel.id)
-        {
-            button2.id->
-            {
-                buVal+="-"+buVal
-
-            }
-            button5.id->
-            {
-                buVal+="7"
-
-            }
-            button6.id->
-            {
-                buVal+="8"
-
-            }
-            button7.id->
-            {
-                buVal+="9"
-
-            }
-            button9.id->
-            {
-                buVal+="4"
-
-            }
-            button10.id->
-            {
-                buVal+="5"
-
-            }
-            button11.id->
-            {
-                buVal+="6"
-
-            }
-            button13.id->
-            {
-                buVal+="1"
-
-            }
-            button14.id->
-            {
-                buVal+="2"
-
-            }
-            button15.id->
-            {
-                buVal+="3"
-
-            }
-            button17.id->
-            {
-                buVal+="0"
-
-            }
-            button18.id->
-            {
-                buVal+="."
-
-            }
-
-        }
-        etShow.setText(buVal)
-    }
-
 
 }
